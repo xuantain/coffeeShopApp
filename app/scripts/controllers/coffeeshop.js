55e7b20f8a770d352a686c018005ec660eb7421f
xuantain <xuantain@gmail.com>
2015-03-30 21:00:08 +0700

    update method: # addNewShop() # removeShop()

--- app/scripts/controllers/coffeeshop.js
+++ app/scripts/controllers/coffeeshop.js
@@ -8,7 +8,7 @@
  * Controller of the angularProjectApp
  */
 angular.module('angularProjectApp')
-  .controller('CoffeeshopCtrl', function ($scope, localStorageService) {
+  .controller('CoffeeshopCtrl', function ($scope, localStorageService, Common) {
     
     $scope.coffeeShops = {};
   	var shopsInStore = localStorageService.get('coffeeShops');
@@ -17,11 +17,29 @@ angular.module('angularProjectApp')
   		localStorageService.set('coffeeShops', $scope.coffeeShops);
   	}, true);
   	$scope.addNewShop = function() {
-  		$scope.coffeeShops.push($scope.shop);
-  		$scope.shop = '';
-  	};
+      if(Common.isObjJSON($scope.shop) && (undefined !== $scope.shop.title) && 
+        ($scope.shop.title.trim().length !== 0)) {
+        var theSameObjs = [];
+        var title = $scope.shop.title.toLowerCase();
+        if(title.indexOf('coffee') > -1) {
+          title.replace('coffee', '');
+        }
+        angular.forEach($scope.coffeeShops, function(coffeeShop) {
+          if(coffeeShop.title.search(title)) {
+            theSameObjs.push(coffeeShop);
+          }
+        });
+        if(theSameObjs.length > 0) {
+      		
+        } else {
+          $scope.coffeeShops.push($scope.shop);
+          $scope.shop = '';
+        }
+  	}};
   	$scope.removeShop = function(index) {
-  		$scope.coffeeShops.splice(index, 1);
+      if((-1 < index) && (index < $scope.coffeeShops.length)) {
+  		  $scope.coffeeShops.splice(index, 1);
+      }
   	};
 
   });
