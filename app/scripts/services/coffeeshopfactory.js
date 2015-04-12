0d761405ec494cfbc5da30fa63642f5f770d4e25
cesine <cesine@yahoo.com>
2015-04-12 21:58:57 +0700

    for @8 using facotry pattern to add functionality to coffee shops"

--- app/scripts/services/coffeeshopfactory.js
+++ app/scripts/services/coffeeshopfactory.js
@@ -8,6 +8,27 @@
  * Service in the coffeeShopApp.
  */
 angular.module('coffeeShopApp')
-  .service('CoffeeShopFactory', function () {
+  .service('CoffeeShopFactory', function() {
     // AngularJS will instantiate a singleton by calling "new" on this function
+
+    var coffeeShopFunctionality = {
+      save: function() {
+        this.dateModified = Date.now();
+        console.log("saving")
+      }
+    };
+
+    return {
+      create: function(options) {
+        var shopWithFunctionality = Object.create(coffeeShopFunctionality);
+
+        for (var property in options) {
+          if (options.hasOwnProperty(property)) {
+            shopWithFunctionality[property] = options[property];
+          }
+        }
+
+        return shopWithFunctionality;
+      }
+    };
   });
--- test/spec/services/coffeeshopfactory.js
+++ test/spec/services/coffeeshopfactory.js
@@ -1,18 +1,63 @@
 'use strict';
 
-describe('Service: CoffeeShopFactory', function () {
+describe('Service: CoffeeShopFactory', function() {
 
   // load the service's module
   beforeEach(module('coffeeShopApp'));
 
   // instantiate service
   var CoffeeShopFactory;
-  beforeEach(inject(function (_CoffeeShopFactory_) {
+  beforeEach(inject(function(_CoffeeShopFactory_) {
     CoffeeShopFactory = _CoffeeShopFactory_;
   }));
 
-  it('should do something', function () {
-    expect(!!CoffeeShopFactory).toBe(true);
+  it('should be accept a json object', function() {
+    var shop1 = CoffeeShopFactory.create({
+      'id': 'abczyzitem1',
+      'title': 'Highland',
+      'address': 'Bach Dang',
+      'phone': '0905xxxyyy',
+      'country': 'vn',
+      'city': 'Da Nang',
+      'district': 'Hai Chau',
+      'mainImage': 0,
+      '_attachments': {},
+      'menu': {
+        'cafe sua': {},
+        'tra da': {}
+      },
+      'dateCreate': '2015-04-01',
+      'isOperational': 'true',
+      'rate': '3'
+    });
+
+    expect(shop1).toBeDefined();
+    expect(shop1.address).toEqual('Bach Dang');
+  });
+
+  it('should be able to save', function() {
+    var shop1 = CoffeeShopFactory.create({
+      'id': 'abczyzitem1',
+      'title': 'Highland',
+      'address': 'Bach Dang',
+      'phone': '0905xxxyyy',
+      'country': 'vn',
+      'city': 'Da Nang',
+      'district': 'Hai Chau',
+      'mainImage': 0,
+      '_attachments': {},
+      'menu': {
+        'cafe sua': {},
+        'tra da': {}
+      },
+      'dateCreate': '2015-04-01',
+      'isOperational': 'true',
+      'rate': '3'
+    });
+
+    expect(shop1.save).toBeDefined();
+    shop1.save();
+    expect(shop1.dateModified).toBeDefined();
   });
 
 });
