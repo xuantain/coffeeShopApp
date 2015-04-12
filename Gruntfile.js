83ef7b64f68d4e563283c73cb3663235a3052eec
MatMath <mathieu.k.legault@gmail.com>
2015-04-12 22:26:41 +0700

    Working grunt task to push sample json to localhost

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,10 +7,11 @@
 // use this if you want to recursively match all subfolders:
 // 'test/spec/**/*.js'
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('grunt-couch');
 
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
@@ -347,9 +348,20 @@ module.exports = function (grunt) {
       }
     },
 
+    'couch-push': {
+      localhost: {
+        options: {
+          user: 'admin',
+          pass: 'none'
+        },
+        files: {
+          'http://localhost:5984/coffee_shops': 'sample_file/couchData.json'
+        }
+      }
+    },
   });
 
-  grunt.registerTask('serve', function (target) {
+  grunt.registerTask('serve', function(target) {
     if (target === 'dist') {
       return grunt.task.run(['build', 'connect:dist:keepalive']);
     }
@@ -364,7 +376,7 @@ module.exports = function (grunt) {
     ]);
   });
 
-  grunt.registerTask('server', function (target) {
+  grunt.registerTask('server', function(target) {
     grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
     grunt.task.run(['serve:' + target]);
   });
@@ -399,4 +411,10 @@ module.exports = function (grunt) {
     'test',
     'build'
   ]);
+
+  grunt.registerTask('deployCouch', function() {
+    grunt.task.run('couch-push:localhost');
+  });
+
+
 };
