18b8ef60a10647053ec40ce63b9768c7e542d685
xuantain <xuantain@gmail.com>
2015-05-28 11:11:06 +0700

    1. Create 3 new test case for method coffeeShopFactory.update() 2. Create 1 new test case for method coffeeShopFactory.addNewMenuItem()

--- test/spec/services/coffeeshopfactory.js
+++ test/spec/services/coffeeshopfactory.js
@@ -143,6 +143,16 @@ describe('Service: CoffeeShopFactory', function() {
       expect(shop1.menu['whiteCoffee']).toBe(menuItem1);
     });
 
+    it('should not able to add new menuItem with no name', function() {
+      var menuItem1 = MenuItemFactory.create({
+        'price': '7000', 
+        "currency": "VND"
+      }); 
+
+      expect(shop1.addNewMenuItem).toBeDefined();
+      expect(shop1.addNewMenuItem('', menuItem1)).toBe(2);
+    });
+
     it('should not able to add a menuItem is conflicted', function() {
       var menuItem1 = MenuItemFactory.create({
         'price': '7000', 
@@ -176,6 +186,89 @@ describe('Service: CoffeeShopFactory', function() {
 
   describe('Test functionality update()',function(){
     
+    var shop1;
+
+    beforeEach(function() {
+      shop1 = CoffeeShopFactory.create({
+        'title': 'Highland',
+        'address': 'Bach Dang',
+        'phone': '0905xxxyyy',
+        'locate': {
+          'country': 'vn',
+          'city': 'Da Nang', 
+          'latitude': '',
+          'longitude': ''
+        },
+        'mainImage': 0,
+        '_attachments': {},
+        'menu': {
+          'cafe sua': {
+            "price": "15000", 
+            "currency": "VND"
+          }
+        },
+        'dateCreate': '2015-04-01',
+        'isOperational': 'true',
+        'openingTimes': '07:00-22:00',
+        'rate': '3'
+      });
+    });
+
+    it('should not able to update coffeeShop with blank title', function(){
+      var updatePros = {
+        'title': ''
+      };
+
+      // var errorObj = new Error();
+      // errorObj.code = 2;
+      // errorObj.message = 'title is invalid';
+
+      expect(shop1.update).toBeDefined();
+      // expect(shop1.update(updatePros)).toThrow(errorObj);
+      expect(shop1.update(updatePros)).toBe(2);
+      expect(shop1.title).not.toBeNull();
+    });
+
+    it('should be able to update coffeeShop successfully but with warnings', function(){
+      var updatePros = {
+        'address': '',
+        'phone': '',
+        'locate': {
+          'country': '',
+          'city': ''
+        }
+      };
+
+      expect(shop1.update).toBeDefined();
+      expect(shop1.update(updatePros)).toBe(1);
+      expect(shop1.title).toEqual('Highland');
+      expect(shop1.address).toBeNull();
+      expect(shop1.phone).toBeNull();
+      expect(shop1.locate.country).toBeNull();
+      expect(shop1.locate.city).toBeNull();
+    });
+
+    it('should be able to update coffeeShop successfully', function(){
+      var updatePros = {
+        'address': 'Hai Phong',
+        'phone': '0905abcxyz',
+        'locate': {
+          'latitude': '16.069885',
+          'longitude': '108.224942'
+        }
+      };
+
+      expect(shop1.update).toBeDefined();
+      expect(shop1.update(updatePros)).toBe(0);
+      expect(shop1.title).toEqual('Highland');
+      expect(shop1.address).toEqual('Hai Phong');
+      expect(shop1.phone).toEqual('0905abcxyz');
+      expect(shop1.locate.country).not.toBeNull();
+      expect(shop1.locate.city).not.toBeNull();
+      expect(shop1.locate.latitude).not.toBeNull();
+      expect(shop1.locate.longitude).not.toBeNull();
+    });
+
   });
 
 });
